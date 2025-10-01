b118ab247cbeb68cff5d5d3127a2fc3c53df87cf
iqbd3 <abod3868@gmail.com>
2025-10-01 21:14:22 +0300

    Delete Lab 4.5.py

--- Lab 4.5.py
+++
@@ -1,9 +0,0 @@
-i = 0
-while i != 1:
-    username = str(input("Enter your username: "))
-    password = str(input("Enter your password: "))
-    if str(username) == "ITC" and str(password) == "P@ssword":
-        print ("welcome you can use the system now !")
-        i = i + 1
-    else :
-        print("Wrong username or password")
