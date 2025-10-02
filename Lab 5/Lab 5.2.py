1f1bd6f4b061383ffd9a769b344f17dc54805353
iqbd3 <abod3868@gmail.com>
2025-10-02 11:28:42 +0300

    Lab 5.2.py

--- Lab 5/Lab 5.2.py
+++ Lab 5/Lab 5.2.py
@@ -1,14 +1,14 @@
 digit = 0
-numbers = ("0123456789")
-Cin = input("Enter a number: ")
-for i in Cin:
-    if i in numbers:
-        digit += 1
-if digit == 1 or digit == 2:
-    print(digit, "digit")
-elif digit > 2:
-    print(digit , "digits")
-else:
-    print("not a number")
+all_digits = '1234567890'
+number = input("Enter a number")
+for x in number:
+    if x in all_digits :
+        digit = digit + 1
 
+if digit == 1 or digit == 2 :
+    print (digit , "digit")
+elif digit >= 2 :
+    print (digit , "digits")
+else :
+    print ("not a number")
 
